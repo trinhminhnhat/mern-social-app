6e3d9b77abf881d2a7fc53569dafbf3e838978a1
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2022-11-23 21:39:17 +0700

    feat: implement api for user

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import authRoutes from './auth.route.js';
+import userRoutes from './user.route.js';
 
 const routes = express.Router();
 
 routes.use(authRoutes);
+routes.use(userRoutes);
 
 export default routes;
