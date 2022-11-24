6844ffa9a0d35cb9df9aff79db83f144362b7bb6
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2022-11-24 21:19:15 +0700

    feat: implement api for post

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import authRoutes from './auth.route.js';
+import postRoutes from './post.route.js';
 import userRoutes from './user.route.js';
 
 const routes = express.Router();
 
 routes.use(authRoutes);
 routes.use(userRoutes);
+routes.use(postRoutes);
 
 export default routes;
