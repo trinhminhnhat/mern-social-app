76c3e14b3109ff213a29be7eb2960e74aa4dd704
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2022-11-26 10:37:28 +0700

    feat: setup react router dom. redux, redux-toolkit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,38 @@
+import { configureStore } from '@reduxjs/toolkit';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+import authReducer from './state';
+
+const persistConfig = { key: 'root', storage, version: '1.0.0' };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => {
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        });
+    },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistStore(store)}>
+                <App />
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
