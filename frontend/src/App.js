94d4262194243b7edeb48a3f0d63c3378267c9a4
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2022-11-26 15:45:52 +0700

    feat: theme setting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,28 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
 import Homepage from 'pages/Homepage';
 import Login from 'pages/Login';
 import Profile from 'pages/Profile';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { themeSettings } from './theme';
 
 function App() {
+    const mode = useSelector((state) => state.mode);
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
     return (
         <div className="app">
             <BrowserRouter>
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/" element={<Homepage />} />
-                    <Route path="/profile/:userId" element={<Profile />} />
-                </Routes>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Routes>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/" element={<Homepage />} />
+                        <Route path="/profile/:userId" element={<Profile />} />
+                    </Routes>
+                </ThemeProvider>
             </BrowserRouter>
         </div>
     );
